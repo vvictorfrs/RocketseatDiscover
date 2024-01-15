368fcf098cff45fd4d37f3f89378b8b949cb5007
Victor Farias <victor.farias@elogroup.com.br>
2024-01-15 19:02:20 -0300

    Ajuste css e botao js light

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,1 +1,24 @@
-alert("Olá")
+function toggleMode() {
+  const html = document.documentElement
+
+  /*  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  } */
+
+  // alterar o fundo light/padrão do css
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
